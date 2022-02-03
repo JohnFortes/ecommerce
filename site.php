10ef8461dd70c9534dae340c901ec5b399ce5c13
JohnFortes <john.fortes01@gmail.com>
2022-02-03 18:54:51 -0300

    Carrinho de Compras

--- site.php
+++ site.php
@@ -4,6 +4,7 @@ use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -59,6 +60,15 @@ $app->get("/products/:desurl", function($desurl){
 		'categories'=>$product->getCategories()
 	]);
 
+});
+
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
 });
 
 
