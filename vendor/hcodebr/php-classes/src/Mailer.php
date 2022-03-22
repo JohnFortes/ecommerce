cc87e73093a1a1fdafa2ebaa5de80d0c1aebc82b
JohnFortes <john.fortes01@gmail.com>
2022-03-22 17:40:24 -0300

    Minha conta

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -7,7 +7,7 @@ use Rain\Tpl;
 class Mailer{
 
 	const USERNAME = "johnlbfortes@gmail.com";
-	const PASSWORD = "testeteste";
+	const PASSWORD = "teste";
 	const NAME_FROM = "JS cases";
 
 	private $mail;
