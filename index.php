b63f50d2e31a9f1a7a9d6f8a70fe96fd8574d431
JohnFortes <john.fortes01@gmail.com>
2022-04-10 11:34:05 -0300

    Admin - Pedidos

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
+/*
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
+*/
 
 $app = new Slim();
 
@@ -20,6 +22,7 @@ require_once("admin.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
 require_once("admin-products.php");
+require_once("admin-orders.php");
 
 $app->run();
 
